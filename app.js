376ca51341e8e93d9c2a234ab1813df62fdecf6d
Aliou Diallo <aliou.diallo@me.com>
2013-08-09 14:14:29 +0200

    Add ugly geolocation button.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 var map = L.mapbox.map('map', 'aliou.map-jimsg0cq');
+
+var locate_me = document.getElementById('locateme');
+
+if (navigator.geolocation) {
+  locate_me.onclick = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    map.locate();
+  };
+}
+
+map.on('locationfound', function(e) {
+  map.fitBounds(e.bounds);
+
+  map.markerLayer.setGeoJSON({
+    type: "Feature",
+    geometry: {
+      type: "Point",
+    coordinates: [e.latlng.lng, e.latlng.lat]
+    },
+    properties: {
+      'marker-color': '#000',
+      'marker-symbol': 'star-stroked'
+    }
+  });
+
+  // And hide the geolocation button
+  locate_me.parentNode.removeChild(locate_me);
+});
